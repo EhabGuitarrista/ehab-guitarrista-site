0e08bd2190b7004f6783a207c7ab4ae252da9581
EhabGuitarrista <ehab.guitarrista@gmail.com>
2025-09-20 23:32:50 -0700

    mm

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-// import Music from "@/components/Music";
+import Music from "@/components/Music";
 import LivePerformances from "@/components/LivePerformances";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -12,7 +12,7 @@ const Index = () => {
       <Navigation />
       <Hero />
       <About />
-      {/* <Music /> */}
+      <Music />
       <LivePerformances />
       <Contact />
       <Footer />
